63b0cc019b9d83597144eaa6d007de27ad5f6454
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-02-06 19:05:17 +0700

    Hoàn thành bài 2

---
+++ Bai2_Bien_ChuyenDoiNhietDo/src/BaiTap2.java
@@ -0,0 +1,11 @@
+
+public class BaiTap2 {
+
+	public static void main(String[] args) {
+		//F = (C * 9/5) + 32
+		double c = 25.0;
+		double f = (c * 9 / 5) + 32;
+		System.out.println(c + " độ C = " + f + " độ F");
+	}
+
+}
