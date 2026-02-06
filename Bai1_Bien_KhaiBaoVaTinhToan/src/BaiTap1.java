44c9c21156c02b43ce1f77d171c0786e5e3140b5
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-02-06 18:34:44 +0700

    Hoàn thành bài 1

---
+++ Bai1_Bien_KhaiBaoVaTinhToan/src/BaiTap1.java
@@ -0,0 +1,16 @@
+
+public class BaiTap1 {
+
+	public static void main(String[] args) {
+		int a = 15;
+		int b = 4;
+		double c = 3.5;
+		int tong = a + b;
+		double thuong = (double) a / b;
+		int phan_du = a;
+		System.out.println("Tong: " + tong);
+		System.out.println("Thuong: " + thuong);
+		System.out.println("Phan du: " + phan_du);
+	}
+
+}
