4ac9b37b229150ef218e8df9e5c2f05c4581800e
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-02-08 21:02:50 +0700

    Hoàn thành bài 1 BTVN

--- 1BTVN_Bai2_TinhTienLuong/src/BaiTapVeNha1.java
+++ 1BTVN_Bai2_TinhTienLuong/src/BaiTapVeNha1.java
@@ -2,7 +2,14 @@
 public class BaiTapVeNha1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int gio = 45;
+		int luong = 30000;
+		double tong;
+		if (gio <= 40)
+			tong = (double) luong * gio;
+		else
+			tong = (double) luong * 40 + luong * (gio - 40) * 1.5;
+		System.out.println("Luong: " + tong);
 
 	}
 
