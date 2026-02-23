2009186884eea3e0362d141a9b3b350e9d21b7bb
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-02-23 16:42:58 +0700

    Hoàn thành bài 3 BTVN

---
+++ 3BTVN_Bai2_GiaiPhuongTrinhBac1/src/BaiTapVeNha3.java
@@ -0,0 +1,14 @@
+
+public class BaiTapVeNha3 {
+
+	public static void main(String[] args) {
+		//a * x + b == 0
+		int a = 3;
+		int b = 6;
+		double x = (double) (-b) / a;
+		System.out.println("Nghiem cua phuong trinh " + a + " * x + " + b + " la: " + x);
+		
+
+	}
+
+}
