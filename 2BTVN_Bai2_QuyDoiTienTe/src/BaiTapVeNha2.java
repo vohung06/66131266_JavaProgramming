90a43ab93c83d731f4ab70e35c781621ae9ec24b
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-02-23 16:32:33 +0700

    Hoàn thành bài 2 BTVN

--- 2BTVN_Bai2_QuyDoiTienTe/src/BaiTapVeNha2.java
+++ 2BTVN_Bai2_QuyDoiTienTe/src/BaiTapVeNha2.java
@@ -2,8 +2,13 @@
 public class BaiTapVeNha2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int usd = 20;
+		int eur = 30;
+		int vnd;
+		vnd = usd * 23500;
+		System.out.println(usd + " USD = " + vnd + " VND");
+		vnd = eur * 27000;
+		System.out.println(eur + " EUR = " + vnd + " VND");
 	}
 
 }
