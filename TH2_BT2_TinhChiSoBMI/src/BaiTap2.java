f2728a49c39af1056be99abdb557023c412b4483
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-03-03 22:45:12 +0700

    Đang làm bài 2

---
+++ TH2_BT2_TinhChiSoBMI/src/BaiTap2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class BaiTap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+	}
+
+}
