f2031dad4fab8511f850d3fdefe955b25f299183
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-03-04 22:19:26 +0700

    Đang làm 1 xíu

---
+++ TH3_BTVN3_NhatKyCaNhan/src/BaiTapVeNha3.java
@@ -0,0 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BaiTapVeNha3 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
