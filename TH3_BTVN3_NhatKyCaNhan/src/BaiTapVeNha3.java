4bbdabc994e506da042eb0bc26e9c11cdce3d95f
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-03-04 22:35:28 +0700

    Đã hoàn thành

--- TH3_BTVN3_NhatKyCaNhan/src/BaiTapVeNha3.java
+++ TH3_BTVN3_NhatKyCaNhan/src/BaiTapVeNha3.java
@@ -1,11 +1,34 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class BaiTapVeNha3 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.print("Hay nhap noi dung dien vao nhap ky: ");
+        String noiDung = scanner.nextLine();
+        try {
+        	FileWriter writer = new FileWriter("nhatky.txt", true);
+        	writer.write(noiDung + "\n");
+        	writer.close();
+        }
+        catch (IOException e) {
+        	System.out.println("Loi ghi file: " + e.getMessage());
+        }
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("nhatky.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Loi doc file: " + e.getMessage());
+        }
+        scanner.close();
 
 	}
 
