53ee551a9f37d5d69a2fbebd6fa99e9608224036
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-03-10 22:15:21 +0700

    Hoàn thành

--- EX_Circle_1_1/src/TestCircle.java
+++ EX_Circle_1_1/src/TestCircle.java
@@ -2,7 +2,12 @@
 public class TestCircle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Circle c1 = new Circle();
+		System.out.println("Hinh tron 1 co ban kinh la " + c1.getRadius() + " va dien tich la " + c1.getArea());
+		
+		Circle c2 = new Circle(2.0);
+		System.out.println("Hinh tron 2 co ban kinh la " + c2.getRadius() + " va dien tich la " + c2.getArea());
+		
 
 	}
 
