e3853450a724e43d0a53a6aa995cb162e53f1989
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-03-11 09:46:26 +0700

    xong class invoiceitem

--- EX_Circle_1_5/src/InvoiceItem.java
+++ EX_Circle_1_5/src/InvoiceItem.java
@@ -1,4 +1,46 @@
 
 public class InvoiceItem {
-
+	private String id;
+	private String desc;
+	private int qty;
+	private double unit_price;
+	
+	public InvoiceItem(String id, String desc, int qty, double unit_price) {
+		this.id = id;
+		this.desc = desc;
+		this.qty = qty;
+		this.unit_price = unit_price;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getDesc() {
+		return desc;
+	}
+	
+	public int getQty() {
+		return qty;
+	}
+	
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+	public double getUnitPrice() {
+		return unit_price;
+	}
+	
+	public void setUnitPrice(double unit_price) {
+		this.unit_price = unit_price;
+	}
+	
+	public double getTotal() {
+		return qty * unit_price;
+	}
+	
+	public String toString() {
+		return "InvoiceItem [id = " + id + ", desc = " + desc + ", qty = " + qty + ", unitPrice = " + unit_price + "]";
+	}
 }
