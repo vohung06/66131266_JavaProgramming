3751cd6a4848880a984bd2eff17b3c8b1157ba60
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-03-24 16:37:12 +0700

    viết main và test

---
+++ OOP_ViDu3_KeThuaQLNV/src/gh/Main.java
@@ -0,0 +1,12 @@
+package gh;
+
+public class Main {
+
+	public static void main(String[] args) {
+		NhanVien nv1 = new NhanVien("001", "Vo Tran Gia Hung", 120000);
+		nv1.hienThiThongTin();
+		System.out.println(nv1);
+
+	}
+
+}
