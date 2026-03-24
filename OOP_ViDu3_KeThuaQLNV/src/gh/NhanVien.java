ea8a748138532356053ad6230d723b97f3e45aac
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-03-24 16:32:10 +0700

    xong class NhanVien

--- OOP_ViDu3_KeThuaQLNV/src/gh/NhanVien.java
+++ OOP_ViDu3_KeThuaQLNV/src/gh/NhanVien.java
@@ -1,5 +1,30 @@
 package gh;
 
 public class NhanVien {
-
+	private String maNV;
+	private String hoTen;
+	private double luongCoBan;
+	
+	public NhanVien() {
+		
+	}
+	public NhanVien(String maNhanVien, String hoTenNhanVien, double luongCoBan) {
+		maNV = maNhanVien;
+		hoTen = hoTenNhanVien;
+		this.luongCoBan = luongCoBan;
+	}
+	
+	public double tinhLuong() {
+		return luongCoBan;
+	}
+	@Override
+	public String toString() {
+		return "NhanVien [maNV=" + maNV + ", hoTen=" + hoTen + ", luongCoBan=" + luongCoBan + "]";
+	}
+	
+	public void hienThiThongTin() {
+		System.out.println("Mã NV: " + maNV);
+        System.out.println("Họ tên: " + hoTen);
+        System.out.println("Lương cơ bản: " + luongCoBan);
+	}
 }
