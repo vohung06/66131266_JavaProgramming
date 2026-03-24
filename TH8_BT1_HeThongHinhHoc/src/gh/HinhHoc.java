5ecaa6e6f37fa1d0509cdb677fe751c70c80959e
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-03-24 17:21:56 +0700

    Xong class HinhHoc

--- TH8_BT1_HeThongHinhHoc/src/gh/HinhHoc.java
+++ TH8_BT1_HeThongHinhHoc/src/gh/HinhHoc.java
@@ -1,5 +1,19 @@
 package gh;
 
 public class HinhHoc {
-
+	private String ten;
+	private String mauSac;
+	
+	public double tinhDienTich() {
+		return 0;
+	}
+	
+	public double tinhChuVi() {
+		return 0;
+	}
+	
+	public void hienThiThongTin() {
+		System.out.println("Ten: " + ten);
+		System.out.println("Mau sac: " + mauSac);
+	}
 }
