d11d9b0bcf5d6329698829cbc5ccdd1aabcc0aa6
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-01 21:01:28 +0700

    Xong class HocSinh

--- OOP_ViDu4_QLHSArrayList/src/gh/HocSinh.java
+++ OOP_ViDu4_QLHSArrayList/src/gh/HocSinh.java
@@ -1,5 +1,45 @@
 package gh;
 
 public class HocSinh {
+	private String tenHS;
+	private short tuoiHS;
+	private String lopHS;
+	
+	public HocSinh() {
+		
+	}
+	public HocSinh(String tenHS, short tuoiHS, String lopHS) {
+		this.tenHS = tenHS;
+		this.tuoiHS = tuoiHS;
+		this.lopHS = lopHS;
+	}
 
+	public String getTenHS() {
+		return tenHS;
+	}
+
+	public void setTenHS(String tenHS) {
+		this.tenHS = tenHS;
+	}
+
+	public short getTuoiHS() {
+		return tuoiHS;
+	}
+
+	public void setTuoiHS(short tuoiHS) {
+		this.tuoiHS = tuoiHS;
+	}
+
+	public String getLopHS() {
+		return lopHS;
+	}
+
+	public void setLopHS(String lopHS) {
+		this.lopHS = lopHS;
+	}
+	@Override
+	public String toString() {
+		return "HocSinh [tenHS=" + tenHS + ", tuoiHS=" + tuoiHS + ", lopHS=" + lopHS + "]";
+	}
+	
 }
