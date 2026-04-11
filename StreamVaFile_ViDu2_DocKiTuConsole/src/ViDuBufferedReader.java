af675dfb1cd5f19a23984adf84afec8bec1c41e0
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-11 21:49:23 +0700

    Hoàn thành

--- StreamVaFile_ViDu2_DocKiTuConsole/src/ViDuBufferedReader.java
+++ StreamVaFile_ViDu2_DocKiTuConsole/src/ViDuBufferedReader.java
@@ -2,8 +2,14 @@ import java.io.*;
 
 public class ViDuBufferedReader {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		char c;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Nhập chuỗi, giới hạn bởi dấu chấm: ");
+		do {
+			c = (char)br.read();
+			System.out.print(c);
+		} while(c != '.');
 
 	}
 
