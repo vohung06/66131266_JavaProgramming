73fb702deb3bd95d109ab6fcc9a11da83e810759
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-11 21:30:26 +0700

    Hoàn thành

--- StreamVaFile_ViDu1_DocChuoi/src/gh/ViDuDocStream.java
+++ StreamVaFile_ViDu1_DocChuoi/src/gh/ViDuDocStream.java
@@ -1,10 +1,19 @@
 package gh;
 
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException; 
+
 public class ViDuDocStream {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		InputStreamReader ipReader = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(ipReader);
+		
+		System.out.print("Nhập một chuỗi bất kỳ: ");
+		String lineDocDuoc = br.readLine();
+		System.out.println("Chuỗi đã nhập: " + lineDocDuoc);
+		
 	}
 
 }
