22634927094c81964f8dfdd2211cfff8ffca3659
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-12 17:06:33 +0700

    Tạo dự án mới, code class PhanSo

---
+++ LuyenTapThem_Bai2_TinhToanPhanSo/src/gh/PhanSo.java
@@ -0,0 +1,59 @@
+package gh;
+
+public class PhanSo {
+	private int tu;
+	private int mau;
+	
+	public PhanSo() {
+		
+	}
+	
+	public PhanSo(int tu, int mau) {
+		this.tu = tu;
+		this.mau = mau;
+	}
+
+	public int getTu() {
+		return tu;
+	}
+
+	public void setTu(int tu) {
+		this.tu = tu;
+	}
+
+	public int getMau() {
+		return mau;
+	}
+
+	public void setMau(int mau) {
+		this.mau = mau;
+	}
+	
+	//Hàm lấy giá trị phân số
+	public float getGiaTriPhanSo() {
+		return (float) tu/mau;
+	}
+
+	@Override
+	public String toString() {
+		return "Phân số; " + tu + "/" + mau;
+	}
+	
+	//Hàm tìm UCLN của tử và mẫu
+	public int UCLN(int a, int b) {
+		while (mau != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return Math.abs(a);
+	}
+	
+	public void rutGon() {
+		int temp = UCLN(tu, mau);
+		tu /= temp;
+		mau /= temp;
+	}
+	
+	
+}
