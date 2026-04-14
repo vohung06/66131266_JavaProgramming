7b890d21d11df9209c244e0d78df8db45f63e160
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-14 23:14:54 +0700

    code 90% circle

---
+++ EX_6_1_AbstractSuperclassShape/src/gh/Circle.java
@@ -0,0 +1,41 @@
+package gh;
+
+public class Circle extends Shape{
+	private double radius;
+
+	public Circle() {
+		
+	}
+
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	
+	public Circle(String color, Boolean filled, double radius) {
+		super(color, filled);
+		this.radius = radius;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	public double getArea() {
+		return radius * radius * Math.PI;
+	}
+	
+	public double getPerimeter() {
+		return radius * 2 * Math.PI;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [Shape[color=" + color + ",filled=" + filled + "],radius=" + radius + "]";
+	}
+	
+	
+}
