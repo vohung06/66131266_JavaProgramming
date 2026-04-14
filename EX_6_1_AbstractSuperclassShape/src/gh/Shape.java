4f44d764636ebf90996037c582b32761e836f2c2
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-14 23:03:27 +0700

    Xong class Shape

---
+++ EX_6_1_AbstractSuperclassShape/src/gh/Shape.java
@@ -0,0 +1,38 @@
+package gh;
+
+public abstract class Shape {
+	private String color;
+	private Boolean filled;
+	
+	public Shape() {
+		
+	}
+	
+	public Shape(String color, Boolean filled) {
+		this.color = color;
+		this.filled = filled;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Boolean getFilled() {
+		return filled;
+	}
+
+	public void setFilled(Boolean filled) {
+		this.filled = filled;
+	}
+
+	@Override
+	public String toString() {
+		return "Shape [color=" + color + ", filled=" + filled + "]";
+	}
+	
+	
+}
