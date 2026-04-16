de1d2a7dd6e1df2667ddaea35e7ceecbe7f4404a
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-16 22:12:50 +0700

    Đang code phần đọc file

--- StreamVaFile_DocDSSVTuFile/src/gh/Main.java
+++ StreamVaFile_DocDSSVTuFile/src/gh/Main.java
@@ -1,16 +1,24 @@
 package gh;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		FileReader fr = new FileReader("SV.csv");
 		BufferedReader br = new BufferedReader(fr);
 		
+		ArrayList<SinhVien> ds = new ArrayList<SinhVien>();
 		
+		while(true) {
+			String line = br.readLine();
+			String[] s = line.split("; ");
+			SinhVien sv = new SinhVien(s[0], s[1], s[2], s[3]);
+		
+		}
 		
 
 	}
