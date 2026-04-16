7c244d3a57dfbae8ca48411d3c77f7927818865b
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-16 22:01:03 +0700

    Tạo class SinhVien

--- StreamVaFile_DocDSSVTuFile/src/gh/Main.java
+++ StreamVaFile_DocDSSVTuFile/src/gh/Main.java
@@ -1,9 +1,17 @@
 package gh;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		FileReader fr = new FileReader("SV.csv");
+		BufferedReader br = new BufferedReader(fr);
+		
+		
+		
 
 	}
 
---
+++ StreamVaFile_DocDSSVTuFile/src/gh/SinhVien.java
@@ -0,0 +1,21 @@
+package gh;
+
+public class SinhVien {
+	private int soThuTu;
+	private String hoTen;
+	private int namSinh;
+	private String gioiTinh;
+	
+	public SinhVien() {
+		
+	}
+	
+	public SinhVien(int soThuTu, String hoTen, int namSinh, String gioiTinh) {
+		this.soThuTu = soThuTu;
+		this.hoTen = hoTen;
+		this.namSinh = namSinh;
+		this.gioiTinh = gioiTinh;
+	}
+	
+	
+}
