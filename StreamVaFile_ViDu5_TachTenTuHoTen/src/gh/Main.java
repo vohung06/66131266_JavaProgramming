87073cfc1ae3d00b56a3d6d7ab27d92acb5deeab
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-17 09:53:36 +0700

    Đã code xong nhưng chưa chạy được

--- StreamVaFile_ViDu5_TachTenTuHoTen/src/gh/Main.java
+++ StreamVaFile_ViDu5_TachTenTuHoTen/src/gh/Main.java
@@ -1,9 +1,30 @@
 package gh;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		FileReader fr = new FileReader("HoTen.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		while(true) {
+			String line = br.readLine();
+			if (line == null)
+				break;
+			String[] s = line.split(" ");
+			int i = 0;
+			while (s[i + 1] != null) {
+				i++;
+			}
+		
+			System.out.println(s[i]);
+			
+		}
+		
+
 
 	}
 
