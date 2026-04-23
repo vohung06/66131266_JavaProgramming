cebbd2ae49624f79884815bf902f4ff0c4b19996
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-23 07:54:37 +0700

    LẦN THỨ 4

---
+++ KTGK_Cau2_VoTranGiaHung_66131266/src/thigk2/votrangiahung/Main.java
@@ -0,0 +1,10 @@
+package thigk2.votrangiahung;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- KTGK_Cau2_VoTranGiaHung_66131266/src/thigk2/votrangiahung/SanPham.java
+++ KTGK_Cau2_VoTranGiaHung_66131266/src/thigk2/votrangiahung/SanPham.java
@@ -1,5 +1,61 @@
 package thigk2.votrangiahung;
 
 public class SanPham {
+	private String maSP;
+	private String tenSP;
+	private String loaiSP;
+	private int giaSP; //thuộc tính thứ 4 do em tự thêm
+	
+	//hàm tạo 
+	public SanPham() {
+		
+	}
+	
+	//hàm tạo với đầy đủ tham số
+	public SanPham(String maSP, String tenSP, String loaiSP, int giaSP) {
+		this.maSP = maSP;
+		this.tenSP = tenSP;
+		this.loaiSP = loaiSP;
+		this.giaSP = giaSP;
+	}
+	
+	//các getter, setter
+	public String getMaSP() {
+		return maSP;
+	}
 
+	public void setMaSP(String maSP) {
+		this.maSP = maSP;
+	}
+
+	public String getTenSP() {
+		return tenSP;
+	}
+
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
+
+	public String getLoaiSP() {
+		return loaiSP;
+	}
+
+	public void setLoaiSP(String loaiSP) {
+		this.loaiSP = loaiSP;
+	}
+
+	public int getGiaSP() {
+		return giaSP;
+	}
+
+	public void setGiaSP(int giaSP) {
+		this.giaSP = giaSP;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
