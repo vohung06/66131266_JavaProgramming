af73a452e72469afc715bbfc2502989aad66f19c
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-23 08:30:51 +0700

    LẦN THỨ 8

---
+++ KTGK_Cau3_VoTranGiaHung_66131266/src/thigk2/votrangiahung/Main.java
@@ -0,0 +1,35 @@
+package thigk2.votrangiahung;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		FileReader fr = new FileReader("SoThuc.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		//không dùng double là kiểu nguyên thuỷ, dùng lớp bao
+		ArrayList<Double> ds = new ArrayList<Double>();
+		
+		while (true) {
+			String line = br.readLine();
+			if (line == null)
+				break;
+			ds.add(Double.parseDouble(line));
+			System.out.println(line);
+		}
+		
+		double X = 10; //ngày sinh của em
+		
+		for (int i = 0; i < ds.size(); i++) {
+			if (ds.get(i) == X)
+				System.out.printf("%.0f có xuất hiện trong danh sách", X);
+		}
+		
+
+	}
+
+}
