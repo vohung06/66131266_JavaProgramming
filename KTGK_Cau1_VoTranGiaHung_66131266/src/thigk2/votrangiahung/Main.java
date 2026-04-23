8053337818d77df93a85b21a3e0e677bfc51d4aa
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-23 07:46:07 +0700

    LẦN THỨ 2

--- KTGK_Cau1_VoTranGiaHung_66131266/src/thigk2/votrangiahung/Main.java
+++ KTGK_Cau1_VoTranGiaHung_66131266/src/thigk2/votrangiahung/Main.java
@@ -1,10 +1,29 @@
 package thigk2.votrangiahung;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Nhập thông tin toạ độ góc Trên - Trái của hình chữ nhật: ");
+		double x1 = sc.nextDouble();
+		double y1 = sc.nextDouble();
+		
+		System.out.println("Nhập thông tin toạ độ góc Dưới - Phải của hình chữ nhật: ");
+		double x2 = sc.nextDouble();
+		double y2 = sc.nextDouble();
+		
+		System.out.println("Toạ độ góc Trên - Trái: (" + x1 + ", " + y1 + ")");
+		System.out.println("Toạ độ góc Dưới - Phải: (" + x2 + ", " + y2 + ")");
+		
+		double canh1 = Math.abs(x2 - x1);
+		double canh2 = Math.abs(y2 - y1);
+		double dienTich = canh1 * canh2;
+		
+		System.out.println("=> Dien tich hinh chu nhat la: " + dienTich);
 	}
 
 }
