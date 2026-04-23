4fa597501de00f930827d0394e72f5731300da45
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-04-23 08:09:27 +0700

    LẦN THỨ 6

--- KTGK_Cau2_VoTranGiaHung_66131266/src/thigk2/votrangiahung/Main.java
+++ KTGK_Cau2_VoTranGiaHung_66131266/src/thigk2/votrangiahung/Main.java
@@ -1,5 +1,8 @@
 package thigk2.votrangiahung;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -11,7 +14,33 @@ public class Main {
 		System.out.println("Sản phẩm 2: " + sp2.toString());
 		System.out.println("Sản phẩm 3: " + sp3.toString());
 		
+		Scanner sc = new Scanner(System.in);
+		
+		SanPham sp4 = new SanPham();
+		
 		System.out.println("Nhập thông tin sản phẩm thứ 4: ");
+		System.out.println(" - Nhập mã sản phẩm: ");
+		String maSP4 = sc.nextLine();
+		sp4.setMaSP(maSP4);
+		System.out.println(" - Nhập tên sản phẩm: ");
+		String tenSP4 = sc.nextLine();
+		sp4.setTenSP(tenSP4);
+		System.out.println(" - Nhập loại sản phẩm: ");
+		String loaiSP4 = sc.nextLine();
+		sp4.setLoaiSP(loaiSP4);
+		System.out.println(" - Nhập tên sản phẩm: ");
+		int giaSP4 = sc.nextInt();
+		sp4.setGiaSP(giaSP4);
+		
+		ArrayList<SanPham> ds = new ArrayList<SanPham>();
+		ds.add(sp1);
+		ds.add(sp2);
+		ds.add(sp3);
+		ds.add(sp4);
+		for(SanPham sp : ds) {
+			System.out.println(sp.toString());
+		}
+		
 	}
 
 }
