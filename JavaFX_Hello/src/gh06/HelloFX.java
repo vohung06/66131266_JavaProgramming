3d6ee81f1715e6593bcb4f3d668f3b8b3ba4c489
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-05-07 08:49:04 +0700

    thực hành

--- JavaFX_Hello/src/gh06/HelloFX.java
+++ JavaFX_Hello/src/gh06/HelloFX.java
@@ -1,9 +1,36 @@
 package gh06;
 
-public class HelloFX {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloFX extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//Tạo nút bấm
+		Button btn = new Button();
+		btn.setText("Xin chào JavaFX");
+		
+		//Tạo cửa sổ
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		
+		//Màn hình
+		Scene scn = new Scene(root, 400, 200);
+		
+		primaryStage.setTitle("Hello World!");
+		primaryStage.setScene(scn);
+		primaryStage.show();
+		
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		launch(args);
 
 	}
 
