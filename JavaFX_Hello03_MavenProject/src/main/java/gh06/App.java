87aeb4434765d5ee18b66ed8a64ecefa512d3153
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-05-11 16:00:09 +0700

    đổi tên các bài tập

---
+++ JavaFX_Hello03_MavenProject/src/main/java/gh06/App.java
@@ -0,0 +1,37 @@
+package gh06;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+    	Button btn = new Button();
+		btn.setText("Xin chào JavaFX");
+		
+		//Tạo cửa sổ
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		
+		//Màn hình
+		Scene scn = new Scene(root, 400, 200);
+		
+		primaryStage.setTitle("Hello World!");
+		primaryStage.setScene(scn);
+		primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
