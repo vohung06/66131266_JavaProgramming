ff5292a2b611fe787ecc76d523b3b4083738c5a1
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-05-11 09:40:37 +0700

    Hoàn thành

--- JavaFX_Hello02/src/gh06/Main.java
+++ JavaFX_Hello02/src/gh06/Main.java
@@ -1,26 +1,37 @@
 package gh06;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		//Tạo nút bấm
+		Button btn = new Button();
+		btn.setText("Xin chào JavaFX");
+		
+		//Tạo cửa sổ
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		
+		//Màn hình
+		Scene scn = new Scene(root, 400, 200);
+		
+		primaryStage.setTitle("Hello World!");
+		primaryStage.setScene(scn);
+		primaryStage.show();
+		
+		
+		
 	}
-	
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		launch(args);
+
 	}
+
 }
