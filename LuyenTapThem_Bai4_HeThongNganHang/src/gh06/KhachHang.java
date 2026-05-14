c96535ade9c3f27f39e4538bcd83b501f910d04b
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-05-14 11:54:30 +0700

    bổ sung các thuộc tính, phương thức

--- LuyenTapThem_Bai4_HeThongNganHang/src/gh06/KhachHang.java
+++ LuyenTapThem_Bai4_HeThongNganHang/src/gh06/KhachHang.java
@@ -1,5 +1,62 @@
 package gh06;
 
 public class KhachHang {
+	String maKH;
+	String hoTen;
+	String sdt;
+	String diaChi;
+	
+	public KhachHang() {
+		
+	}
 
+	public KhachHang(String maKH, String hoTen, String sdt, String diaChi) {
+		this.maKH = maKH;
+		this.hoTen = hoTen;
+		this.sdt = sdt;
+		this.diaChi = diaChi;
+	}
+
+	public String getMaKH() {
+		return maKH;
+	}
+
+	public void setMaKH(String maKH) {
+		this.maKH = maKH;
+	}
+
+	public String getHoTen() {
+		return hoTen;
+	}
+
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+
+	public String getSdt() {
+		return sdt;
+	}
+
+	public void setSdt(String sdt) {
+		this.sdt = sdt;
+	}
+
+	public String getDiaChi() {
+		return diaChi;
+	}
+
+	public void setDiaChi(String diaChi) {
+		this.diaChi = diaChi;
+	}
+
+	@Override
+	public String toString() {
+		return "KhachHang [maKH=" + maKH + ", hoTen=" + hoTen + ", sdt=" + sdt + ", diaChi=" + diaChi + "]";
+	}
+	
+	
+	
+	
+	
+	
 }
