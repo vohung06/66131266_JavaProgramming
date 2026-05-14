a4b5ad9edf273fdd58a41a142a2d5dd8dd47f256
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-05-14 12:08:42 +0700

    khởi tạo lớp TaiKhoan

--- LuyenTapThem_Bai4_HeThongNganHang/src/gh06/TaiKhoan.java
+++ LuyenTapThem_Bai4_HeThongNganHang/src/gh06/TaiKhoan.java
@@ -1,5 +1,46 @@
 package gh06;
 
 public class TaiKhoan {
+	private String stk;
+	private double soDu;
+	private KhachHang chuTK;
+	
+	public TaiKhoan() {
+	}
 
+	public TaiKhoan(String stk, double soDu, KhachHang chuTK) {
+		super();
+		this.stk = stk;
+		this.soDu = soDu;
+		this.chuTK = chuTK;
+	}
+
+	public String getStk() {
+		return stk;
+	}
+
+	public void setStk(String stk) {
+		this.stk = stk;
+	}
+
+	public double getSoDu() {
+		return soDu;
+	}
+
+	public void setSoDu(double soDu) {
+		this.soDu = soDu;
+	}
+
+	public KhachHang getChuTK() {
+		return chuTK;
+	}
+
+	public void setChuTK(KhachHang chuTK) {
+		this.chuTK = chuTK;
+	}
+	
+	
+	
+	
+	
 }
