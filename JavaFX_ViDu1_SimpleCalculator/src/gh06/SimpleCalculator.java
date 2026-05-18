c4fae4e45fe3081b74fa44413ba8b38403e6d7be
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-05-18 18:57:27 +0700

    tạo các nút cộng, trừ, nhân, chia

--- JavaFX_ViDu1_SimpleCalculator/src/gh06/SimpleCalculator.java
+++ JavaFX_ViDu1_SimpleCalculator/src/gh06/SimpleCalculator.java
@@ -1,8 +1,11 @@
 package gh06;
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class SimpleCalculator {
@@ -17,5 +20,13 @@ public class SimpleCalculator {
 		tfNumber1.setPrefColumnCount(3);
 		tfNumber2.setPrefColumnCount(3);
 		tfResult.setPrefColumnCount(3);
+		
+		pane.getChildren().addAll(new Label("Number 1"), tfNumber1, new Label("Number 2"), tfNumber2, new Label("Result: "), tfResult);
+		
+		HBox hBox = new HBox(5);
+		Button btAdd = new Button("Add");
+		Button btSub = new Button("Subtract");
+		Button btMul = new Button("Multioly");
+		Button btDiv = new Button("Divide");
 	}
 }
