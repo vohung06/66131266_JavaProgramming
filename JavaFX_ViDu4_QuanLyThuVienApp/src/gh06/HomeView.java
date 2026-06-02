d51c93a3e9ac7658fd468f88100b7dbbfabced25
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-06-02 11:38:41 +0700

    tạo header trang home

--- JavaFX_ViDu4_QuanLyThuVienApp/src/gh06/HomeView.java
+++ JavaFX_ViDu4_QuanLyThuVienApp/src/gh06/HomeView.java
@@ -1,12 +1,28 @@
 package gh06;
 
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class HomeView {
 	public Scene createScene(Stage stage) {
-		VBox root = new VBox();
+		BorderPane root = new BorderPane();
+		
+		Label lbTitle = new Label("QUẢN LÝ THƯ VIỆN");
+		
+		Image img = new Image("file:images/header_home.png");
+		ImageView iv = new ImageView(img);
+		iv.setFitHeight(150);
+		iv.setFitWidth(150);
+		
+		HBox headerBox = new HBox(20);
+		headerBox.getChildren().addAll(lbTitle, iv);
+	
+		
 		return new Scene(root, 500, 350);
 	}
 }
