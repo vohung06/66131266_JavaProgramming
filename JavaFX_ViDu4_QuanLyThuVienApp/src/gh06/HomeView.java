14e51e0fdfb1345766c9530328cb77e4c29886f3
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-06-02 11:55:52 +0700

    tạo các đối tượng btn

--- JavaFX_ViDu4_QuanLyThuVienApp/src/gh06/HomeView.java
+++ JavaFX_ViDu4_QuanLyThuVienApp/src/gh06/HomeView.java
@@ -1,6 +1,7 @@
 package gh06;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -12,17 +13,29 @@ public class HomeView {
 	public Scene createScene(Stage stage) {
 		BorderPane root = new BorderPane();
 		
-		Label lbTitle = new Label("QUẢN LÝ THƯ VIỆN");
-		
 		Image img = new Image("file:images/header_home.png");
 		ImageView iv = new ImageView(img);
-		iv.setFitHeight(150);
-		iv.setFitWidth(150);
+		iv.setFitHeight(80);
+		iv.setFitWidth(80);
 		
+		Label lbTitle = new Label("QUẢN LÝ THƯ VIỆN");
+				
 		HBox headerBox = new HBox(20);
-		headerBox.getChildren().addAll(lbTitle, iv);
-	
+		headerBox.getChildren().addAll(iv, lbTitle);
 		
+		Button btnAdd = new Button("Thêm sách");
+		Button btnList = new Button("Danh sách sách");
+		Button btnLogOut = new Button("Đăng xuất");
+		
+		HBox btnBox = new HBox(15);
+		btnBox.getChildren().addAll(btnAdd, btnList, btnLogOut);
+			
+		root.setTop(headerBox);
+		root.setLeft(btnAdd);
+		root.setCenter(btnList);
+		root.setRight(btnLogOut);
+		
+		lbTitle.setStyle("-fx-font-weight:bold; -fx-text-fill:navy; -fx-font-size: 20px");
 		return new Scene(root, 500, 350);
 	}
 }
