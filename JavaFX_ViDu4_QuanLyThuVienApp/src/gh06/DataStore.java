1efeba80cbc29bb7801cdfe46ec5d00b748c0189
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-06-02 22:11:31 +0700

    tạo class lưu dữ liệu

---
+++ JavaFX_ViDu4_QuanLyThuVienApp/src/gh06/DataStore.java
@@ -0,0 +1,11 @@
+package gh06;
+
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class DataStore {
+	public static ArrayList<Book> books = new ArrayList<>();
+	
+}
--- JavaFX_ViDu4_QuanLyThuVienApp/src/module-info.java
+++ JavaFX_ViDu4_QuanLyThuVienApp/src/module-info.java
@@ -1,6 +1,7 @@
 module JavaFX_ViDu4_QuanLyThuVienApp {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens gh06 to javafx.graphics, javafx.fxml;
 }
