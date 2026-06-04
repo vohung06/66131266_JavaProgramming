8eeb0923c0980fa86ee9e3b53ebde9235b438459
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-06-04 16:15:20 +0700

    xong class Mammal

--- EX_4_6_SuperAnimal/src/gh06/Main.java
+++ EX_4_6_SuperAnimal/src/gh06/Main.java
@@ -6,7 +6,8 @@ public class Main {
 		Animal a1 = new Animal("Pig");
 		System.out.println(a1);
 		
-
+		Mammal m1 = new Mammal("Dolphin");
+		System.out.println(m1);
 	}
 
 }
---
+++ EX_4_6_SuperAnimal/src/gh06/Mammal.java
@@ -0,0 +1,22 @@
+package gh06;
+
+public class Mammal extends Animal {
+
+	public Mammal() {
+		super();
+	}
+
+	public Mammal(String name) {
+		super(name);
+	}
+
+	@Override
+	public String toString() {
+		return "Mammal[" + super.toString() + "]";
+	}
+	
+	
+	
+	
+	
+}
