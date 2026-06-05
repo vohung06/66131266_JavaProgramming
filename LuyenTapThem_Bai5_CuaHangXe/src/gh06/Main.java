4aa3b3e04b1c5d45a857e2d741e6d389a0f14bce
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-06-05 12:44:08 +0700

    sắp xếp theo giá lăn bánh

--- LuyenTapThem_Bai5_CuaHangXe/src/gh06/Main.java
+++ LuyenTapThem_Bai5_CuaHangXe/src/gh06/Main.java
@@ -1,19 +1,53 @@
 package gh06;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Xe x1 = new Xe("Mecedes", 2, LocalDate.of(2025,10,20), 1000000);
-		Xe x2 = new Xe();
+	public static void main(String[] args) throws IOException {
+		FileReader fr = new FileReader("DanhSachXe.txt");
+		BufferedReader br = new BufferedReader(fr);
 		
-		x2.nhap();
+		ArrayList<XeVinfast> ds = new ArrayList<>();
 		
-		System.out.println("\n");
-		x1.xuat();
-		x2.xuat();
+		while(true) {
+			String line = br.readLine();
+			if (line == null)
+				break;
+			
+			String s[] = line.split(";");
+			XeVinfast temp = new XeVinfast(s[0], Integer.parseInt(s[1]), LocalDate.parse(s[2]), Integer.parseInt(s[3]), s[4]);
+			ds.add(temp);			
+		}
 		
+		for (XeVinfast x : ds) {
+			x.xuat();
+		}
+		
+		//Sắp xếp theo chiều giảm dần của giá lăn bánh
+		for (int i = 0; i < ds.size() - 1; i++) {
+		    int min = i;
+
+		    for (int j = i + 1; j < ds.size(); j++) {
+		        if (ds.get(j).tinhGiaBan() < ds.get(min).tinhGiaBan()) {
+		            min = j;
+		        }
+		    }
+
+		    XeVinfast temp = ds.get(i);
+		    ds.set(i, ds.get(min));
+		    ds.set(min, temp);
+		}
+		
+		System.out.println("Danh sách xe sau khi sắp xếp theo chiều giảm dần giá lăn bánh: ");
+		for (XeVinfast x : ds) {
+			x.xuat();
+			System.out.println(" - Giá lăn bánh: " + x.tinhGiaLanBanh());
+		}
 		
 
 	}
